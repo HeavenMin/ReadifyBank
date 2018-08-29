b95e3463d714d86b58d7d509012ac725cb379b1d
HeavenMin <ming1@student.unimelb.edu.au>
2018-08-29 11:50:41 +1000

    modify fun program

--- ReadifyBank.ConsoleApp/Program.cs
+++ ReadifyBank.ConsoleApp/Program.cs
@@ -3,10 +3,11 @@
   Date: 08/2018
   Versin: 1
   File Name: Program.cs
-  Purpose: demonstrate
+  Purpose: demonstrate, have some fun.
 */
 
 using System;
+using System.Collections.Generic;
 using ReadifyBank;
 using ReadifyBank.Interfaces;
 using System.Diagnostics;
@@ -26,10 +27,11 @@ namespace ReadifyBank.ConsoleApp
                 bank.OpenSavingsAccount("SCVustomer");
                 bank.OpenHomeLoanAccount("LNCustomer");
             }
-            Console.WriteLine(string.Format("Total account number: {0}\n", bank.AccountList.Count));
+            Console.WriteLine(string.Format("After open 20 account. Total account number: {0}\n",
+                                             bank.AccountList.Count));
+
             showCustomerInfo(john, bank);
             showCustomerInfo(jack, bank);
-
             foreach (int i in Enumerable.Range(1, 10))
             {
                 bank.PerformDeposit(john, 100, "deposit 100");
@@ -49,21 +51,59 @@ namespace ReadifyBank.ConsoleApp
             showCustomerInfo(jack, bank);
 
             bank.PerformTransfer(john, jack, 50, "lunch fee.");
-            Console.WriteLine("After john transfer 50 to jack:");
+            Console.WriteLine("After John transfer 50 to Jack:");
             showCustomerInfo(john, bank);
             showCustomerInfo(jack, bank);
 
-            Console.WriteLine("John transfor amount exceeds balance:");
+            Console.WriteLine("When John transfor amount exceeds balance:");
             bank.PerformTransfer(john, jack, 800, "bill.");
             showCustomerInfo(john, bank);
             showCustomerInfo(jack, bank);
+
+            Console.WriteLine("Calculate interest for 60 days for John and Jack:");
+            DateTimeOffset toDate = DateTimeOffset.Now.LocalDateTime.AddDays(60);
+            decimal interestForJohn = bank.CalculateInterestToDate(john, toDate);
+            decimal interestForJack = bank.CalculateInterestToDate(jack, toDate);
+            Console.WriteLine(string.Format("The interest for John to 60 days later is: {0:C4}", interestForJohn));
+            Console.WriteLine(string.Format("The interest for Jack to 60 days later is: {0:C4}\n", interestForJack));
+
+            IEnumerable<IStatementRow> miniStatementOfJohn = bank.GetMiniStatement(john);
+            IEnumerable<IStatementRow> miniStatementOfJack = bank.GetMiniStatement(jack);
+            Console.WriteLine("Mini statement of John:");
+            foreach (IStatementRow transaction in miniStatementOfJohn)
+            {
+                showTransactionInfo(transaction, bank);
+            }
+            Console.WriteLine("\nMini statement of Jack:");
+            foreach (IStatementRow transaction in miniStatementOfJack)
+            {
+                showTransactionInfo(transaction, bank);
+            }
+
+            Console.WriteLine("\nNow close the account John:");
+            IEnumerable<IStatementRow> allTransactionOfJohn = bank.CloseAccount(john);
+            foreach (IStatementRow transaction in allTransactionOfJohn)
+            {
+                showTransactionInfo(transaction, bank);
+            }
 
         }
 
         private static void showCustomerInfo(IAccount account, ReadifyBank bank)
         {
-            Console.WriteLine(string.Format("Customer {0} info:\nName: {0}\nAccount: {1}\nBalance: {2}\nOpened date: {3}\n",
-                                             account.CustomerName, account.AccountNumber, account.Balance, account.OpenedDate));
+            Console.WriteLine(string.Format("Customer {0} info:\nName: {0}, Account: {1}, " + 
+                                            "Balance: {2}\nOpened date: {3}\n",
+                                            account.CustomerName, account.AccountNumber, 
+                                            account.Balance, account.OpenedDate));
+        }
+
+        private static void showTransactionInfo(IStatementRow transaction, ReadifyBank bank)
+        {
+            Console.WriteLine(string.Format("Transaction info: Account: {0}, Date: {1}, " + 
+                                            "Amount: {2}, Balance: {3}, Description: {4}", 
+                                            transaction.Account.AccountNumber, transaction.Date,
+                                            transaction.Amount, transaction.Balance, 
+                                            transaction.Description));
         }
     }
 }
